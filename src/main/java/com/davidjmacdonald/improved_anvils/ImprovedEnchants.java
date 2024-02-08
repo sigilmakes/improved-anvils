f0786c50d9010bcf05c9b5b6abd0967d0098b0f7
david <davidjmacdonald11@gmail.com>
2024-02-08 09:15:12 -0500

    Remove incompatibility sets in favor of built-in canCombine method

--- src/main/java/com/davidjmacdonald/improved_anvils/ImprovedEnchants.java
+++ src/main/java/com/davidjmacdonald/improved_anvils/ImprovedEnchants.java
@@ -9,7 +9,6 @@ import java.util.*;
 
 public class ImprovedEnchants {
     private static final Map<Enchantment, Integer> MAX_COSTS = new HashMap<>();
-    private static final Set<Set<Enchantment>> INCOMPATIBILITY_SETS = new HashSet<>();
 
     private static void putEnchant(Enchantment e, int maxCost) {
         MAX_COSTS.put(e, maxCost);
@@ -63,54 +62,26 @@ public class ImprovedEnchants {
         putEnchant(Enchantments.SMITE, 300);
         putEnchant(Enchantments.IMPALING, 300);
         putEnchant(Enchantments.BANE_OF_ARTHROPODS, 250);
-
-        // Incompatibility sets
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.SHARPNESS, Enchantments.SMITE, Enchantments.BANE_OF_ARTHROPODS));
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.PROTECTION, Enchantments.PROJECTILE_PROTECTION, Enchantments.BLAST_PROTECTION, Enchantments.FIRE_PROTECTION));
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.FORTUNE, Enchantments.SILK_TOUCH));
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.FROST_WALKER, Enchantments.DEPTH_STRIDER));
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.INFINITY, Enchantments.MENDING));
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.MULTISHOT, Enchantments.PIERCING));
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.RIPTIDE, Enchantments.LOYALTY));
-        INCOMPATIBILITY_SETS.add(Set.of(Enchantments.RIPTIDE, Enchantments.CHANNELING));
     }
 
     private static int getCost(Enchantment e, int level) {
         return MAX_COSTS.getOrDefault(e, 300) * level / e.getMaxLevel();
     }
 
-    private static Set<Enchantment> getIncompatibilitySet(Enchantment e) {
-        var set = new HashSet<Enchantment>();
-        for (var s : INCOMPATIBILITY_SETS) {
-            if (s.contains(e)) {
-                set.addAll(s);
-            }
-        }
-
-        return set;
-    }
-
     private final Map<Enchantment, Integer> enchants;
-    private final Set<Enchantment> incompatibleSet;
 
     public ImprovedEnchants(ItemStack item) {
         this.enchants = new HashMap<>(EnchantmentHelper.get(item));
-        this.incompatibleSet = new HashSet<>();
-
-        for (var e : this.enchants.keySet()) {
-            this.incompatibleSet.addAll(getIncompatibilitySet(e));
-        }
     }
 
     public int add(Enchantment enchant, int level) {
         var cost = getCost(enchant, level);
 
         if (!this.enchants.containsKey(enchant)) {
-            if (this.incompatibleSet.contains(enchant)) {
+            if (!canAdd(enchant)) {
                 return 0;
             }
 
-            this.incompatibleSet.addAll(getIncompatibilitySet(enchant));
             this.enchants.put(enchant, level);
             return cost;
         }
@@ -120,16 +91,22 @@ public class ImprovedEnchants {
             return 0;
         }
 
-        if (level == oldLevel) {
-            level++;
-        }
-
-        this.enchants.put(enchant, level);
+        this.enchants.put(enchant, level + ((level == oldLevel) ? 1 : 0));
         return cost;
     }
 
     public void set(ItemStack item) {
         EnchantmentHelper.set(this.enchants, item);
+    }
+
+    private boolean canAdd(Enchantment e2) {
+        for (var e1 : this.enchants.keySet()) {
+            if (!e1.canCombine(e2)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
